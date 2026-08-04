cf8095c0f67f49aecfb61fb7e1583eac31b2a8f9
Ozge Stenstrom <you@example.com>
2026-08-04 07:48:01 +0200

    andra commit

---
+++ Models/Topic.cs
@@ -0,0 +1,11 @@
+
+using System.Collections.Generic;
+
+public class Topic
+{
+    public string Id { get; set; } = "";
+
+    public string Title { get; set; } = "";
+
+    public List<WordItem> Words { get; set; } = new List<WordItem>();
+}
---
+++ Models/WordItem.cs
@@ -0,0 +1,9 @@
+
+public class WordItem
+{
+    public string Id { get; set; } = "";
+    public string Text { get; set; } = "";
+    public string ImagePath { get; set; } = "";
+    public string AudioPath { get; set; } = "";
+    public string Description { get; set; } = "";
+}
